4e807fbdc8eb484d4dad9e8df38519610d7d740b
Morel Frédéric <frederic.morel.ars@outlook.fr>
2022-06-21 16:03:23 +0400

    first commit

---
+++ partie 1/App.js
@@ -0,0 +1,23 @@
+/* Etape 1 crée une variable de type erray qui va contenir des objet qui ressemble a{nom:string,age:number,sexe:tring } */
+
+
+
+
+/* Étape 2 affiches les infos sous forme de liste en utilisant ul présent dans html du projet  */
+
+
+
+
+
+
+
+/* Etap3 3 créé input de recherche que tu vas injecter dans le dom en js avec un appenchild  */
+
+
+
+
+
+
+
+
+/* Étape   4 utilisée la propriété du type array qui permet de recherche avec un filter les éléments qui correspond a ce que utilisateur va entrée dans l'input de recherche   */
