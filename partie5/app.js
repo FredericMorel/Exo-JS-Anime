5aa29d6c1a34df088514b8da69a58fd5b23d2ee3
Frédéric Morel <frederic.morel.ars@outlook.fr>
2022-06-27 11:37:59 +0400

    test for Map object and Set object

---
+++ partie5/app.js
@@ -0,0 +1,8 @@
+const players = new Map();
+
+players.set(1, { name: "alexis", position: "defense" });
+players.set(2, { name: "Thomas", position: "middle" });
+console.log(players.get(1));
+console.log(players.get(2));
+
+const players2 = [{}];
