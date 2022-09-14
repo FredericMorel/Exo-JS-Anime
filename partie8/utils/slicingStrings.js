be6213c8c333fb6897ef4dbee9a24ae08dadf6a3
Morel Frédéric <frederic.morel.ars@outlook.fr>
2022-09-14 14:18:45 +0400

    restore last version

--- partie8/utils/slicingStrings.js
+++ partie8/utils/slicingStrings.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 function slicingString(text,len) {
     if(text.length>len){
         text=text.slice(0,len)
         text=text+'...'
     }
     return text
-=======
-function slicingString(text,len) {
-    if(text.length>len){
-        text=text.slice(0,len)
-        text=text+'...'
-    }
-    return text
->>>>>>> 60252362a37c3b69534fba4ae21b3006d8c1f266
 }
